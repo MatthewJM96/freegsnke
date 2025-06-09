2d215bea839a521e6b49867685764d41526f6016
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2025-06-09 11:30:24 +0100

    Implement a start up routine using posix-ipc to create the shared memory segment and semaphores required to communicate with the RTVC server.

--- freegsnke/control_loop/rtvc_provider.py
+++ freegsnke/control_loop/rtvc_provider.py
@@ -1,9 +1,26 @@
 import logging
+import mmap
 from pathlib import Path
+
+from posix_ipc import Semaphore, SharedMemory, O_CREAT, O_EXCL, unlink_semaphore, unlink_shared_memory, BusyError, ExistentialError, SEMAPHORE_TIMEOUT_SUPPORTED
 
 from .vc_provider import VirtualCircuitProvider
 
 _logger = logging.getLogger(__name__)
+
+
+_SHARED_MEMORY_NAME = "/rtvc_shm"
+_SHARED_MEMORY_SIZE = 1024
+_SEM_READY_NAME = "/rtvc_inf_req"
+_SEM_DONE_NAME = "/rtvc_inf_done"
+_SEM_QUIT_NAME = "/rtvc_quit"
+
+
+if not SEMAPHORE_TIMEOUT_SUPPORTED:
+    _logger.warning(
+        "Semaphore timeout is not supported on this system, as such a failure of the "
+        "RTVC server to start up will result in this program stalling."
+    )
 
 
 class RealTimeVirtualCircuitProvider(VirtualCircuitProvider):
@@ -32,3 +49,64 @@ class RealTimeVirtualCircuitProvider(VirtualCircuitProvider):
             else:
                 _logger.error("Failed to start RTVC server.")
 
+    def start_up(self) -> bool:
+        """
+        Creates the IPC resources needed to communicate with the RTVC server, and then
+        starts that server up.
+        """
+
+        # Create a shared memory segment with name _SHARED_MEMORY_NAME and size
+        # _SHARED_MEMORY_SIZE. Under the hood, posix_ipc uses `shm_open` and `ftruncate`
+        # to achieve this.
+
+        try:
+            self._shared_memory = SharedMemory(name=_SHARED_MEMORY_NAME, flags=O_CREAT | O_EXCL, size=_SHARED_MEMORY_SIZE)
+        except ExistentialError:
+            _logger.error("Shared memory segment already exists for RTVC comms.")
+            return False
+
+        # Map the shared memory segment just created into the address space of this
+        # process. This gives us a pointer at which location we can set data to pass to
+        # the RTVC server for VC prediction.
+
+        self._shared_memory_ptr = mmap.mmap(self._shared_memory.fd, _SHARED_MEMORY_SIZE)
+
+        # Create a set of semaphores used for communicating between this process and the
+        # RTVC server. These semaphores have the following responsibilities:
+        #   Ready:  when signalled with a call to `Semaphore.release` by this process,
+        #           the RTVC server is signalled to predict a VC given the data
+        #           presently stored in the shared memory segment.
+        #   Done:   when a signal is recieved with a call to `Semaphore.acquire` in this
+        #           process, the RealTimeVirtualCircuitProvider class is informed that
+        #           the RTVC server has completed prediction and the new VC is available
+        #           to be read from the shared memory segment. Note that we also use
+        #           a call to `Semaphore.acquire` on the Done semaphore to receive the
+        #           alive signal from the RTVC server during its start-up.
+        #   Quit:   when signalled with a call to `Semaphore.release` by this process,
+        #           the RTVC server is signalled to shutdown. Note that a final shutdown
+        #           actually requires one more call to `Semaphore.release` on the Ready
+        #           Semaphore to avoid a race condition.
+
+        try:
+            self._sem_ready = Semaphore(name=_SEM_READY_NAME, flags=O_CREAT | O_EXCL, initial_value=0)
+            self._sem_done = Semaphore(name=_SEM_DONE_NAME, flags=O_CREAT | O_EXCL, initial_value=0)
+            self._sem_quit = Semaphore(name=_SEM_QUIT_NAME, flags=O_CREAT | O_EXCL, initial_value=0)
+        except ExistentialError:
+            _logger.error("Semaphores already exist for RTVC comms.")
+            return False
+
+        # TODO(Matthew): Start RTVC server.
+
+        # Await an alive signal from the RTVC server, if not received in the timeout
+        # period we log an error as we consider the RTVC server dead.
+        try:
+            # Timeout of 10 seconds for RTVC server to start up, more than necessary for
+            # present RTVC implementation.
+            self._sem_done.acquire(timeout=10)
+        except BusyError:
+            _logger.error(
+                "Could not communicate with RTVC server, likely it failed to start up."
+            )
+            return False
+
+        return True
